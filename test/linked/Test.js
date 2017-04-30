90c9645440d680e28501801a732f7b48e43b6d22
twipixel <twpixel@naver.com>
2017-04-30 20:57:32 +0900

    연결 리스트 예제 작성 중

--- test/linked/Test.js
+++ test/linked/Test.js
@@ -4,6 +4,8 @@ import {
     cancelAnimationFrame,
     Easing
 } from './../../external/lib/animation';
+import Listener from './listener/Listener';
+
 
 export default class Test
 {
@@ -22,18 +24,56 @@ export default class Test
 
     initialize()
     {
-        console.log('Linked List');
+        // TEST ID
+        window.listenerId = 0;
+
+        this._first = null;
         this._numListeners = 0;
-        this._first = undefined;
+        this._listeners = [];
+
+        // 기본 리스너 등록
+        this.addListener(new Listener());
     }
 
     initializeGUI()
     {
         this.gui = new dat.GUI();
+
+        this.config = {};
+        this.config.addListener = this.testAddListener.bind(this);
+        this.config.removeListener = this.testRemoveListener.bind(this);
+        this.config.displayListeners = this.displayListeners.bind(this);
+
+        this.gui.add(this.config, 'addListener');
+        this.gui.add(this.config, 'removeListener');
+        this.gui.add(this.config, 'displayListeners');
     }
 
+    testAddListener()
+    {
+        this.addListener(new Listener());
+    }
+
+    testRemoveListener()
+    {
+        var randomIndex = parseInt(Math.random() * this._listeners.length);
+        var listener = this._listeners[randomIndex];
+        console.log('\ntestRmoveListener, id:', listener.id);
+        this.removeListener(listener);
+    }
+
+    displayListeners()
+    {
+        console.log('total listener:', this._listeners.length);
+        for (var i = 0; i < this._listeners.length; i++) {
+            var listener = this._listeners[i];
+            console.log(listener.id);
+        }
+    }
+
+
     /**
-     * @param listener {TickerListener}
+     * @param listener {Listener}
      */
     addListener(listener)
     {
@@ -53,14 +93,20 @@ export default class Test
         this._first = listener;
 
         ++this._numListeners;
+
+
+        // Test Code: 디버그를 위해 리스트에 넣고 뺍니다.
+        this._listeners.push(listener);
+        console.log('addListener(' + listener.id + '), numListener:', this._numListeners);
     }
 
     /**
      *
-     * @param listener {TickerListener}
+     * @param listener {Listener}
      */
     removeListener(listener)
     {
+        console.log('removeListener(', listener.id, ')');
         var l = this._first;
 
         while (l != null) {
@@ -78,20 +124,36 @@ export default class Test
                     l.prevListener = null;
                 }
                 --this._numListeners;
+
+                // Test Code: 디버그를 위해 리스트에 넣고 뺍니다.
+                var total = this._listeners.length;
+                for (var i = 0; i < total; i++) {
+                    var member = this._listeners[i];
+
+                    if (member.id == listener.id) {
+                        this._listeners.splice(i, 1);
+                        break;
+                    }
+                }
             }
 
             l = l.nextListener;
         }
+
+
+        console.log('removeListener(' + listener.id + '), numListener:', this._numListeners, ', total:', this._listeners.length);
     }
 
     update(ms)
     {
+        if (this._first == null) return;
+
         var listener = this._first, ll = null;
 
         while (listener.nextListener != null) {
 
             // 리스너 tick 함수가 true 를 리턴하면 제거합니다.
-            if ((listener = listener.nextListener).tick(t)) {
+            if ((listener = listener.nextListener).tick()) {
                 
                 // 리스트 연결
                 if (listener.prevListener != null) {
@@ -109,6 +171,7 @@ export default class Test
                 
                 // 리스너 카운트 줄임
                 --this._numListeners;
+                console.log('_numListeners:', this._numListeners);
             }
         }
     }
--- test/linked/listener/Listener.js
+++ test/linked/listener/Listener.js
@@ -1,15 +1,22 @@
 export default class Listener
 {
-    constructor()
+    constructor(listenerEndCount = 300 + parseInt(Math.random() * 500))
     {
+        this.id = window.listenerId++;
+
         this.tickCount = 0;
-        this.listenerEndCount = 20 + parseInt(Math.random() * 60);
-        console.log('listenerEndCount:', this.listenerEndCount);
+        this.listenerEndCount = listenerEndCount;
+
+        this.prevListener = null;
+        this.nextListener = null;
+
+        console.log('id:' + this.id + ', listenerEndCount:', this.listenerEndCount);
     }
 
     tick()
     {
         if (this.tickCount++ >= this.listenerEndCount) {
+            console.log('this.id:', + this.id + ' end');
             return true;
         }
         return false;
